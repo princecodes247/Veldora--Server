063385bc777974642cdf1d1eb8d7fdce04d90a7e
Prince Codes <princecodes247@gmail.com>
2022-04-14 15:43:38 -0400

    feat: update user controllers

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,40 +1,58 @@
 const User = require("../models/User");
 
-// ------ Fetch Controllers -------------
+
+
 // Get a single user from the database
 const getUser = (userID) => {
-  
-}
+  const user = User.findById(userID);
+  //   check if user exists
+  if (!user) {
+    return {
+      status: 404,
+      message: "User not found",
+    };
+  }
+  return user;
+};
 
 // Get all users owned by a single user
-const getAllUsers = () => {
-    // Fetch all users
-    // .find()
-}
-// --------------------------------------
-
+const getAllUsers = async () => {
+  // Fetch all users
+  const allUsers = await User.find();
+  return allUsers;
+};
 
 // Create a new user
 const createUser = (details) => {
-    
-}
+    const user = new User(details);
+    return user.save();
+};
 
 // Add data to user
 const updateUserProjects = (userID, data) => {
-    // let 
-}
+    const user = getUser(userID);
+    if (user.status === 404) {
+        return user;
+    }
+    user.projects = data;
+    return user.save();
+};
 
-// Delete a single user 
+// Delete a single user
 const deleteUser = (userID) => {
-
-}
+    const user = getUser(userID);
+    if (user.status === 404) {
+        return user;
+    }
+    return user.remove();
+};
 
 AuthController = {
-    createUser,
-    deleteUser,
-    getAllUsers,
-    getUser,
-    updateUserProjects
-}
+  createUser,
+  deleteUser,
+  getAllUsers,
+  getUser,
+  updateUserProjects,
+};
 
-module.exports = AuthController
+module.exports = AuthController;
