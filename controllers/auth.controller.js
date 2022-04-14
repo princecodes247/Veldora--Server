42f10adfa9b3aca77daaa85044a43aef94b09209
Prince Codes <princecodes247@gmail.com>
2022-04-14 15:57:04 -0400

    feat: update auth controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,26 @@
 
 
-const signIn = () => {
-
+const signIn = (details) => {
+    const user = getUserByEmail(details.email);
+    if (user.status === 404) {
+        return user;
+    }
+    // Check if password is correct with passport authentication
+    if (!user.authenticate(details.password)) {
+        return {
+            status: 401,
+            message: "Invalid email or password",
+        };
+    }
+    return user;
+    
 }
 
 const signOut = () => {
-
+    return {
+        status: 200,
+        message: "User signed out",
+    };
 }
 
 const AuthController = {
