1de393e8e6a4e22809505c48459ff076d7ca8a79
Prince Codes <princecodes247@gmail.com>
2022-04-14 15:57:39 -0400

    feat: update project controller

--- controllers/project.controller.js
+++ controllers/project.controller.js
@@ -3,16 +3,34 @@ const Project = require("../models/Project");
 // ------ Fetch Controllers -------------
 // Get a single project from the database
 const getProject = (projectTag) => {
-    let project = Project.findOne(projectTag).then(
-        res => {
-            return res
-        }).catch(err => console.log(err))
+    const project = Project.findById(projectTag);
+    //   check if project exists
+    if (!project) {
+        return {
+            status: 404,
+            message: "Project not found",
+        };
+    }
+    return project;
+}
+
+const getProjectByTag = (projectTag) => {
+    const project = Project.find(projectTag);
+    //   check if project exists
+    if (!project) {
+        return {
+            status: 404,
+            message: "Project not found",
+        };
+    }
+    return project;
 }
 
 // Get all projects owned by a single user
 const getProjectsByUser = (userID) => {
     // Fetch all projects by a single user
-    let projects = Project.find(userID)
+    const allProjects = Project.find(userID);
+    return allProjects;
     
 }
 // --------------------------------------
@@ -20,20 +38,27 @@ const getProjectsByUser = (userID) => {
 
 // Create a new project
 const createProject = (details) => {
-    let newProject = {
-
-    }
-    // Project
+    const project = new Project(details);
+    return project.save();
 }
 
 // Add data to project
 const saveToProject = (projectID, data) => {
-    // let 
+    const project = getProject(projectID);
+    if (project.status === 404) {
+        return project;
+    }
+    project.data = data;
+    return project.save();
 }
 
 // Delete a single project 
 const deleteProject = (projectID) => {
-
+    const project = getProject(projectID);
+    if (project.status === 404) {
+        return project;
+    }
+    return project.remove();
 }
 
 module.exports = {
