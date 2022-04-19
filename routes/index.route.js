0513f86c9c7203d6c560829f28b8bd70dee842d0
Prince Codes <princecodes247@gmail.com>
2022-04-19 18:04:30 -0400

    temp: comment out test sendmail route

--- routes/index.route.js
+++ routes/index.route.js
@@ -7,10 +7,10 @@ router.get('/', function(req, res, next) {
 });
 
 // Temporary route to test email functionality
-const sendEmail = require('../utils/sendEmail')
-router.get('/', function(req, res, next) {
-  sendEmail('princecodes247@gmail.com', 'Hello', 'Hello World')
-  res.send('sent!!!');
-});
+// const sendEmail = require('../utils/sendEmail')
+// router.get('/sendEmail', function(req, res, next) {
+//   sendEmail('princecodes247@gmail.com', 'Hello', 'Hello World')
+//   res.send('sent!!!');
+// });
 
 module.exports = router;
