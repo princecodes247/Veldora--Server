2582d07ad55be250064cd8cfa8b33ff5944f5554
Prince Codes <princecodes247@gmail.com>
2022-04-19 03:26:03 -0400

    testing db connection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 
 const connect = () => {
     const url =
-      process.env.MONGODB_URI || `mongodb://localhost:27017/Veldora`;
+      process.env.MONGODB_URI || `mongodb://localhost:27017/veldora`;
     console.log("Establish new connection with url", url);
     mongoose.Promise = global.Promise;
-    try {
-      mongoose.connect(url);
-    } catch (error) {
-      console.log("Error connecting to database", error);      
-    }
+    // mongoose.set("useNewUrlParser", true);
+    // mongoose.set("useFindAndModify", false);
+    // mongoose.set("useCreateIndex", true);
+    // mongoose.set("useUnifiedTopology", true);
+    mongoose.connect(url);
 }
 
 module.exports = connect
