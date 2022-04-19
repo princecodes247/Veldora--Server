9dc823fdbf76ebe5ea1cd7ed2d66237d672224f4
Prince Codes <princecodes247@gmail.com>
2022-04-19 18:02:51 -0400

    feat: setup nodemailer

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,12 +1,27 @@
 const nodemailer = require('nodemailer')
 
+ // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+//   let testAccount = await nodemailer.createTestAccount();
+
 const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
+    host: 'smtp-relay.sendinblue.com',
     port: 587,
     auth: {
-        user: 'rosalinda.cruickshank77@ethereal.email',
-        pass: 'MuBwgJNbfbAbK9Bbmk'
-    }
+        user: 'princecodes247@gmail.com',
+        pass: process.env.SMTP_PASSWORD
+    },
+    secure: true
 });
+
+// const transporter = nodemailer.createTransport({
+//     host: 'smtp-relay.sendinblue.com',
+//     port: 587,
+//     auth: {
+//         user: process.env.SMTP_EMAIL,
+//         pass: process.env.SMTP_KEY
+//     },
+//     secure: false,
+// });
 
 module.exports = transporter;
