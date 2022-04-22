6929795f3e0647f6cdf0633b7d65114d5bb95218
Prince Codes <princecodes247@gmail.com>
2022-04-22 02:34:08 -0400

    feat: add basic input validation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,43 +1,49 @@
-const UserService = require('../services/user.service')
+const UserService = require("../services/user.service");
 
 const getAllUsers = async (req, res) => {
-    console.log("hit")
-    let result = await UserService.getAllUsers()
-    res.status(result.status).json(result)
-}
+  console.log("hit");
+  let result = await UserService.getAllUsers();
+  res.status(result.status).json(result);
+};
 
 const getUser = async (req, res) => {
-    let { userID } = req.body
-    let result = await UserService.getUser(userID)
-    res.status(result.status).json(result)
-}
+  let { userID } = req.body;
+  let result = await UserService.getUser(userID);
+  res.status(result.status).json(result);
+};
 
 const getUserByEmail = async (req, res) => {
-    let { email } = req.body
-    let result = await UserService.getUserByEmail(email)
-    res.status(result.status).json(result)
-}
+  let { email } = req.body;
+  let result = await UserService.getUserByEmail(email);
+  res.status(result.status).json(result);
+};
 
 const createUser = async (req, res) => {
-    let { userName, email, password } = req.body
+  let { userName, email, password } = req.body;
 
-    let result = await UserService.createUser({userName, email, password})
-    res.status(result.status).json(result)
-}
+  // Check
+  if (!userName.trim() || !email.trim() || !password.trim()) {
+    return res.status(400).json({
+      status: 400,
+      message: "Please provide all required fields",
+    });
+  }
+  let result = await UserService.createUser({ userName, email, password });
+  res.status(result.status).json(result);
+};
 
 const deleteUser = async (req, res) => {
-    let { userID } = req.params
-    let result = await UserService.deleteUser(userID)
-    res.status(result.status).json(result)
-}
+  let { userID } = req.params;
+  let result = await UserService.deleteUser(userID);
+  res.status(result.status).json(result);
+};
 
 const UserController = {
-    createUser,
-    deleteUser,
-    getAllUsers,
-    getUser,
-    getUserByEmail
-}
+  createUser,
+  deleteUser,
+  getAllUsers,
+  getUser,
+  getUserByEmail,
+};
 
-
-module.exports = UserController
+module.exports = UserController;
