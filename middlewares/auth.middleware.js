5d7ebf5eb8acc5b89154b41871d818c1de6531ed
Prince Codes <princecodes247@gmail.com>
2022-04-25 18:09:39 -0400

    add auth snippets

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -12,12 +12,116 @@ const ensureAuthenticated = (req, res, next) => {
   res.redirect("/login");
 };
 
+() => {
+  passport.authenticate('jwt', { session: false })
+}
+
 const forwardAuthenticated = (req, res, next) => {
   if (!req.isAuthenticated()) {
     return next();
   }
   res.redirect("/dashboard");
 };
+
+
+
+passport.use(
+  'signup',
+  new localStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password'
+    },
+    async (email, password, done) => {
+      try {
+        const user = await UserModel.create({ email, password });
+
+        return done(null, user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
+
+passport.use(
+  'login',
+  new localStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password'
+    },
+    async (email, password, done) => {
+      try {
+        const user = await UserModel.findOne({ email });
+
+        if (!user) {
+          return done(null, false, { message: 'User not found' });
+        }
+
+        const validate = await user.isValidPassword(password);
+
+        if (!validate) {
+          return done(null, false, { message: 'Wrong Password' });
+        }
+
+        return done(null, user, { message: 'Logged in Successfully' });
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
+
+// ...
+
+const JWTstrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+
+passport.use(
+  new JWTstrategy(
+    {
+      secretOrKey: 'TOP_SECRET',
+      jwtFromRequest: ExtractJWT.fromUrlQueryParameter('secret_token')
+    },
+    async (token, done) => {
+      try {
+        return done(null, token.user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
+
+passport.authenticate(
+  'login',
+  async (err, user, info) => {
+    try {
+      if (err || !user) {
+        const error = new Error('An error occurred.');
+
+        return next(error);
+      }
+
+      req.login(
+        user,
+        { session: false },
+        async (error) => {
+          if (error) return next(error);
+
+          const body = { _id: user._id, email: user.email };
+          const token = jwt.sign({ user: body }, 'TOP_SECRET');
+
+          return res.json({ token });
+        }
+      );
+    } catch (error) {
+      return next(error);
+    }
+  }
+)(req, res, next);
+
 
 
 module.exports = {
