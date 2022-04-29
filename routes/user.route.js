42729763e8d3cd7f0cff1eff287fe189d6e8eede
Prince Codes <princecodes247@gmail.com>
2022-04-29 08:06:33 -0400

    refactor: clean up user route

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const UserController = require("../controllers/user.controller")
+const UserController = require("../controllers/user.controller");
+const { ensureAuthenticated } = require("../middlewares/auth.middleware");
 
-
-router.get('/getAllUsers',  UserController.getAllUsers);
-router.get('/getUser/:userID',      UserController.getUser);
-router.get('/createUser',   UserController.createUser);
-router.get('/deleteUser/:userID',   UserController.deleteUser);
+router.get("/getAllUsers", ensureAuthenticated, UserController.getAllUsers);
+router.get("/getUser/:userID", ensureAuthenticated, UserController.getUser);
+router.get("/createUser", ensureAuthenticated, UserController.createUser);
+router.get("/deleteUser/:userID", ensureAuthenticated, UserController.deleteUser);
 
 module.exports = router;
