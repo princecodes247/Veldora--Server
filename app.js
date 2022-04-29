8d25b332d5dc666c53edbcb7d0e979409bffc410
Prince Codes <princecodes247@gmail.com>
2022-04-29 08:02:38 -0400

    config passport

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const logger = require('morgan');
+const cors = require('cors');
+const passport = require('passport');
+const session = require("express-session");
 require('dotenv').config()
 
 // DB Config
@@ -10,12 +13,27 @@ require('dotenv').config()
 const connect = require('./config/dbConnection');
 connect();
 
+// Passport Config
+require("./config/passport")(passport);
 
+app.use(
+    session({
+      secret: "secret",
+      resave: true,
+      saveUninitialized: true,
+    })
+  );
+  // Connect flash for flash sessions
+//   app.use(flash());
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Route imports
 const indexRouter = require('./routes/index.route');
 // const adminRouter = require('./routes/admin.route');
-// const authRouter = require('./routes/auth.route');
+const authRouter = require('./routes/auth.route');
 const projectRouter = require('./routes/project.route');
 const userRouter = require('./routes/user.route');
 
@@ -24,7 +42,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// app.use('/auth', authRouter);
+app.use('/auth', authRouter);
 // app.use('/admin', adminRouter);
 app.use('/api/projects', projectRouter);
 app.use('/api/users', userRouter);
