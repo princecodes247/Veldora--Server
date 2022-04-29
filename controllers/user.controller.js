4e7d5284f2ad6a6a695cf3b890fb8d48ff3a8ff1
Prince Codes <princecodes247@gmail.com>
2022-04-29 08:04:29 -0400

    refactor: change userName to username globally

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -19,18 +19,18 @@ const getUserByEmail = async (req, res) => {
 };
 
 const createUser = async (req, res) => {
-  let { userName, email, password } = req.body;
+  let { username, email, password } = req.body;
   password = password.trim()
-  userName = userName.trim()
+  username = username.trim()
   email = email.trim()
   // Check
-  if (!userName || !email || !password) {
+  if (!username || !email || !password) {
     return res.status(400).json({
       status: 400,
       message: "Please provide all required fields",
     });
   }
-  let result = await UserService.createUser({ userName, email, password });
+  let result = await UserService.createUser({ username, email, password });
   res.status(result.status).json(result);
 };
 
