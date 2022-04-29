c971bc2ec8d919c292cea28e7429809d6c285d0c
Prince Codes <princecodes247@gmail.com>
2022-04-29 08:03:40 -0400

    create signup controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,7 @@
-import { getUserByEmail } from './user.controller';
+const passport = require('passport');
+const UserService = require('../services/user.service');
 
-const signIn = async (details) => {
+const signIn = async (req, res, next, details) => {
     const user = await getUserByEmail(details.email);
     if (user.status === 404) {
         return user;
@@ -16,7 +17,56 @@ const signIn = async (details) => {
     
 }
 
+const signUp = async (req, res, next) => {
+    if (!req.body.email || !req.body.password) {
+        res.json({
+            status: 400,
+            message: "Email and password are required",
+        });
+        return
+    }
+    const checkUser = await UserService.getUserByEmail(req.body.email)
+    
+    if (!checkUser.error) {
+        console.log(checkUser.data)
+        res.json({
+            status: 400,
+            message: "Email is already taken",
+        });
+        return
+    }
+    const { email, password, username } = req.body;
+    const user = await UserService.createUser({ email, password, username});
+    console.log(user)
+    if (user.error) {
+
+        res.json({
+            status: user.status,
+            message: user.data,
+        });
+        return
+    }
+    req.login(user.data, (err) => {
+        if (err) {
+            console.log(err);
+            res.json({
+                message: 'Signup successful',
+                user: req.user
+              });
+          }
+          res.json({
+            message: 'Signup successful',
+            user: req.user
+          });
+        });
+}
+
+
+
+
+
 const signOut = () => {
+    req.logout();
     return {
         status: 200,
         message: "User signed out",
@@ -25,7 +75,8 @@ const signOut = () => {
 
 const AuthController = {
     signIn,
-    signOut
+    signOut,
+    signUp,
 }
 
 module.exports = AuthController
