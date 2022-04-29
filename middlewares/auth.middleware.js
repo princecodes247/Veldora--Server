3cef82f6048a264ee0e733bad5584883a05dab9d
Prince Codes <princecodes247@gmail.com>
2022-04-29 08:04:47 -0400

    update auth middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,5 +1,4 @@
 const passport = require('passport');
-const localStrategy = require('passport-local').Strategy;
 const UserService = require('../services/user.service');
 const ensureAdmin = (req, res, next) => {
   if (req.isAuthenticated() && req.user.level == 2) {
@@ -16,7 +15,9 @@ const ensureAdmin = (req, res, next) => {
 // };
 
 const ensureAuthenticated = (req, res, next) => {
-  passport.authenticate('jwt', { session: false })
+  console.log(req.isAuthenticated());
+  console.log(req.user)
+  next()
 }
 
 const forwardAuthenticated = (req, res, next) => {
@@ -26,57 +27,13 @@ const forwardAuthenticated = (req, res, next) => {
   res.redirect("/dashboard");
 };
 
-passport.use(
-  'signup',
-  new localStrategy(
-    {
-      usernameField: 'email',
-      passwordField: 'password'
-    },
-    async (email, password, done) => {
-      try {
-        const user = await UserService.createUser({email, password});
+const signup = passport.authenticate('signup', { session: false })
 
-        return done(null, user);
-      } catch (error) {
-        done(error);
-      }
-    }
-  )
-);
-
-
-passport.use(
-  'login',
-  new localStrategy(
-    {
-      usernameField: 'email',
-      passwordField: 'password'
-    },
-    async (email, password, done) => {
-      try {
-        const user = await UserService.getUserByEmail(email);
-
-        if (!user) {
-          return done(null, false, { message: 'User not found' });
-        }
-
-        const validate = await user.isValidPassword(password);
-
-        if (!validate) {
-          return done(null, false, { message: 'Wrong Password' });
-        }
-
-        return done(null, user, { message: 'Logged in Successfully' });
-      } catch (error) {
-        return done(error);
-      }
-    }
-  )
-);
+const logout = ""
 
 module.exports = {
   ensureAuthenticated,
   ensureAdmin,
-  forwardAuthenticated
+  forwardAuthenticated,
+  signup
 };
