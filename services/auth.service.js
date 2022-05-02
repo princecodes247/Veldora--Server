03e8bd6bb28e3a37c6b15e3f5bffe057c0691da3
Prince Codes <princecodes247@gmail.com>
2022-05-02 09:09:57 -0400

    feat: create auth service

---
+++ services/auth.service.js
@@ -0,0 +1,24 @@
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+const UserService = require("../services/user.service");
+
+const signup = passport.authenticate("signup", { session: false });
+
+const login = async (req, user) => {
+  let token = ""
+  await req.login(user, { session: false }, async (error) => {
+    if (error) return next(error);
+
+    const body = { _id: user.data._id, email: user.data.email };
+    token = await jwt.sign({ user: body }, "TOP_SECRET");
+    console.log("req", token)
+  });
+  console.log("function", token)
+  return token;
+};
+const logout = "";
+
+module.exports = {
+  signup,
+  login,
+};
