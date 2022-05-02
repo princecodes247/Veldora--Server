611b532195f543813ac4841f814874844110152e
Prince Codes <princecodes247@gmail.com>
2022-05-02 19:58:15 -0400

    refactor: rearrange controllers order

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,22 +1,5 @@
 const UserService = require("../services/user.service");
 
-const getAllUsers = async (req, res) => {
-  console.log("hit");
-  let result = await UserService.getAllUsers();
-  res.status(result.status).json(result);
-};
-
-const getUser = async (req, res) => {
-  let { userID } = req.params;
-  let result = await UserService.getUser(userID);
-  res.status(result.status).json(result);
-};
-
-const getUserByEmail = async (req, res) => {
-  let { email } = req.body;
-  let result = await UserService.getUserByEmail(email);
-  res.status(result.status).json(result);
-};
 
 const createUser = async (req, res) => {
   let { username, email, password } = req.body;
@@ -39,6 +22,28 @@ const deleteUser = async (req, res) => {
   let result = await UserService.deleteUser(userID);
   res.status(result.status).json(result);
 };
+
+const getAllUsers = async (req, res) => {
+  console.log("hit");
+  let result = await UserService.getAllUsers();
+  res.status(result.status).json(result);
+};
+
+const getUser = async (req, res) => {
+  let { userID } = req.params;
+  let result = await UserService.getUser(userID);
+  res.status(result.status).json(result);
+};
+
+const getUserByEmail = async (req, res) => {
+  let { email } = req.body;
+  let result = await UserService.getUserByEmail(email);
+  res.status(result.status).json(result);
+};
+
+const updateUserLevel = async (req, res) => {
+  
+}
 
 const UserController = {
   createUser,
