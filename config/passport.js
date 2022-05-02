5f854031e515b587075e3352c3f8c4a38aca9e69
Prince Codes <princecodes247@gmail.com>
2022-05-02 09:08:08 -0400

    update passport config

--- config/passport.js
+++ config/passport.js
@@ -1,64 +1,64 @@
 // Passport configuration
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcrypt');
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
 // Load User Model user.js
-const User = require('../models/User');
-const UserService = require('../services/user.service');
+const User = require("../models/User");
+const UserService = require("../services/user.service");
 
-function passportConfig (passport) {
-// passport.use(new LocalStrategy({
-//     usernameField: 'email',
-//     passwordField: 'password'
-// }, async (email, password, done) => {
-//     // Match user
-//     const user = await User.findOne({ email });
-//     if (err) { return done(err); }
-//     if (!user) {
-//         return done(null, false, { message: 'That email is not registered' });
-//     }
-//     // Match password
-//     const match = await bcrypt.compare(password, user.password);
-//     if (!match) {
-//         return done(null, false, { message: 'Password incorrect' });
-//     }
-//     return done(null, user);
-// }))
-
-  
+function passportConfig(passport) {
   passport.use(
-    'login',
+    "signup",
     new LocalStrategy(
       {
-        usernameField: 'email',
-        passwordField: 'password'
+        usernameField: "email",
+        passwordField: "password",
+      },
+      async (email, password, done) => {
+        try {
+          const user = await UserModel.create({ email, password });
+
+          return done(null, user);
+        } catch (error) {
+          done(error);
+        }
+      }
+    )
+  );
+
+  passport.use(
+    "login",
+    new LocalStrategy(
+      {
+        usernameField: "email",
+        passwordField: "password",
       },
       async (email, password, done) => {
         try {
           const user = await UserService.getUserByEmail(email);
-  
+
           if (!user) {
-            return done(null, false, { message: 'User not found' });
+            return done(null, false, { message: "User not found" });
           }
-  
+
           const validate = await user.isValidPassword(password);
-  
+
           if (!validate) {
-            return done(null, false, { message: 'Wrong Password' });
+            return done(null, false, { message: "Wrong Password" });
           }
-  
-          return done(null, user, { message: 'Logged in Successfully' });
+
+          return done(null, user, { message: "Logged in Successfully" });
         } catch (error) {
           return done(error);
         }
       }
     )
   );
-  
-passport.serializeUser(function(user, cb) {
+
+  passport.serializeUser(function (user, cb) {
     cb(null, user);
   });
-  
-  passport.deserializeUser(function(obj, cb) {
+
+  passport.deserializeUser(function (obj, cb) {
     cb(null, obj);
   });
 }
