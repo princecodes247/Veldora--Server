160330f7db727ccc111741e79543a5cc5db27041
Prince Codes <princecodes247@gmail.com>
2022-05-03 06:30:22 -0400

    feat: setup db connection for production

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,15 +1,19 @@
 const mongoose = require("mongoose");
 
 const connect = () => {
-    const url =
-      process.env.MONGODB_URI || `mongodb://localhost:27017/veldora`;
+    let DB_URI =`mongodb://localhost:27017/veldora`;
+    if (process.env.NODE_ENV === "production") {
+        DB_URI = `mongodb+srv://${process.env.DB_USER}:${
+            process.env.DB_PASS
+        }@cluster0-qjvqw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+    }
     console.log("Establish new connection with url", url);
     mongoose.Promise = global.Promise;
     // mongoose.set("useNewUrlParser", true);
     // mongoose.set("useFindAndModify", false);
     // mongoose.set("useCreateIndex", true);
     // mongoose.set("useUnifiedTopology", true);
-    mongoose.connect(url);
+    mongoose.connect(DB_URI);
 }
 
 module.exports = connect
