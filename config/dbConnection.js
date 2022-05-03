b75d5b0d838deb3c0092a003caf51348f02253c3
Prince Codes <princecodes247@gmail.com>
2022-05-03 06:58:04 -0400

    fix: db error

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -7,7 +7,7 @@ const connect = () => {
             process.env.DB_PASS
         }@cluster0-qjvqw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
     }
-    console.log("Establish new connection with url", url);
+    console.log("Establish new connection with url", DB_URI);
     mongoose.Promise = global.Promise;
     // mongoose.set("useNewUrlParser", true);
     // mongoose.set("useFindAndModify", false);
